889d23d53579ef581f2355bc5fc1b10952b67bba
mostafizurRahaman <fahim654326@gmail.com>
2022-12-08 03:25:42 +0600

    implement recaptcha

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import NoticeTitle from "./NoticeTitle/NoticeTitle";
 import { AiFillWarning } from "react-icons/ai";
 import { WalletContext } from "../../Context/WalletContextProvider";
+import ReCAPTCHA from "react-google-recaptcha";
 const Home = () => {
    const { selectedWallet } = useContext(WalletContext);
+   const [verified, setVerifed] = useState(false); 
+   const onChange = () => {
+         setVerifed(true); 
+   }
+   console.log(process.env.REACT_APP_RECAPTCHA_KEY); 
    return (
       <div className="">
          <NoticeTitle></NoticeTitle>
@@ -22,7 +28,7 @@ const Home = () => {
                <div className="flex w-full p-[10px] text-accent  items-center justify-start gap-2 bg-warning ">
                   <AiFillWarning className="text-xl text-primary "></AiFillWarning>
                   <p className="text-sm ">
-                     Your wallet is connected to
+                     Your wallet  is connected to
                      <span className="text-accent font-bold  ">
                         {selectedWallet.wallet_name
                            ? selectedWallet.wallet_name
@@ -51,32 +57,36 @@ const Home = () => {
                         placeholder="wallet address..."
                      />
                   </div>
-                 
-                     <div className="w-full flex flex-col gap-[5px] capitalize mt-[15px]">
-                        <label
-                           htmlFor="wallet-address"
-                           className="text-[13px] font-semibold text-primary "
-                        >
-                           Result Type
-                        </label>
-                        <div className="flex items-center gap-[15px] w-full">
-                           <input
-                              type="text"
-                              className="w-full border p-[6px] text-sm font-normal capitalize placeholder:text-sm rounded border-gray-200 outline-none bg-gray-100"
-                              readonly 
-                              defaultValue='20 Test Link'
-                              disabled
-                           />
-                           <input
-                              type="text"
-                              className="w-full border p-[6px] text-sm font-normal capitalize placeholder:text-sm rounded border-gray-200 outline-none bg-gray-100"
-                              
-                              readonly 
-                              defaultValue='0.5 ETH'
-                              disabled
-                           />
-                        </div>
-                     
+
+                  <div className="w-full flex flex-col gap-[5px] capitalize mt-[15px]">
+                     <label
+                        htmlFor="wallet-address"
+                        className="text-[13px] font-semibold text-primary "
+                     >
+                        Result Type
+                     </label>
+                     <div className="flex items-center gap-[15px] w-full">
+                        <input
+                           type="text"
+                           className="w-full border p-[6px] text-sm font-normal capitalize placeholder:text-sm rounded border-gray-200 outline-none bg-gray-100"
+                           readonly
+                           defaultValue="20 Test Link"
+                           disabled
+                        />
+                        <input
+                           type="text"
+                           className="w-full border p-[6px] text-sm font-normal capitalize placeholder:text-sm rounded border-gray-200 outline-none bg-gray-100"
+                           readonly
+                           defaultValue="0.5 ETH"
+                           disabled
+                        />
+                     </div>
+                  </div>
+                  <div className="my-[15px]">
+                     <ReCAPTCHA
+                        sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+                        onChange={onChange}
+                     />
                   </div>
                </form>
             </div>

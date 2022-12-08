fd9299b134b1dc238d9ca822a7a047e3f32a1a8f
mostafizurRahaman <fahim654326@gmail.com>
2022-12-08 11:00:52 +0600

    footer section is designed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
    return (
-      <div>
-          <h1>this is our footer page. </h1>
+      <div className='py-[25px] text-center  border border-gray-200 '>
+          <p className='text-[13px] '>&copy; copyright 2022 - All Right Reserved by Faucet</p>
       </div>
    );
 };
